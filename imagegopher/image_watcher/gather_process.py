4105fab3d7f233e2c494d0aceb6b5be6fea14307
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-08-06 15:32:57 +0100

    work on gather process

--- imagegopher/image_watcher/gather_process.py
+++ imagegopher/image_watcher/gather_process.py
@@ -17,28 +17,121 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.If not, see < https://www.gnu.org/licenses/>.
 """
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import logging
+import time
 import typing
 from gather_images import ImageGatherer
+from database_layer import DatabaseLayer
+from state_object import StateObject, ComponentDegradationLevel
 from shared.configuration.configuration import Configuration
 
 ONE_MINUTE_IN_SECONDS: int = 60
 
 
 @dataclass
+class BasePathEntry:
+    id: int
+    path: str
+
+
+@dataclass
 class GatherProcessState:
+    """
+    Represents the state of the gather process, including timing and control flags.
+
+    Attributes:
+        last_process_time (float): Timestamp (e.g., from time.time()) of the last gather operation.
+        refresh_cache (bool): Indicates whether the cache should be forcibly refreshed
+                              during the next gather operation.
+    """
     last_process_time: float = 0
-    refresh_cache: bool = False
+    refresh_cache: bool = True
+    base_paths: list[BasePathEntry] = field(default_factory=list)
+    degraded_state: bool = False
 
 
 class GatherProcess:
     """ Class for the file gathering functionality """
-    __slots__ = ["_config", "_db_layer", "_gatherers", "_logger", "_state",]
+    __slots__ = ["_config", "_db_layer", "_gatherers", "_logger", "_state",
+                 "_state_object"]
 
-    def __init__(self, logger: logging.Logger, config: Configuration) -> None:
+    def __init__(self,
+                 logger: logging.Logger,
+                 config: Configuration,
+                 db_layer: DatabaseLayer,
+                 state_object: StateObject) -> None:
         self._config = config
-        self._db_layer = None   # TEMPORARY UNTIL IMPLEMENTED
+        self._db_layer = db_layer
+        self._state_object = state_object
         self._gatherers: typing.List[ImageGatherer] = []
         self._logger = logger.getChild(__name__)
         self._state = GatherProcessState()
+
+    def process_files(self):
+        """ Attempt to gather image files that are either new or modified """
+
+        # If in fatal degraded state, stop gathering to preserve system
+        if self._state.degraded_state:
+            return
+
+        interval: int = self._config.get_entry("general",
+                                               "directory_scan_interval")
+        start_time: float = time.time()
+        process_now: bool = False
+
+        if self._state.refresh_cache:
+            if not self._cache_base_paths_from_database():
+                self._state.degraded_state = True
+                self._logger.critical(
+                    "Caching base paths failed, service is fully degraded")
+                return
+
+            self._state.refresh_cache = False
+
+        if self._state.last_process_time == 0:
+            self._logger.info("First time execution of image gathering...")
+            process_now = True
+
+        elif start_time - self._state.last_process_time > interval:
+            self._logger.info("Scheduled execution of image gathering...")
+            process_now = True
+
+        if not process_now:
+            return
+
+        for gatherer in self._gatherers:
+            print(gatherer)
+
+        execution_time: float = time.time() - start_time
+        self._logger.info("Execution time : %.3f (seconds)",
+                          execution_time)
+
+        # Always set last process time to current time and not to now because
+        # we don't know how long processing takes.
+        self._state.last_process_time = time.time()
+
+    def _cache_base_paths_from_database(self) -> bool:
+        self._logger.info("Started caching base paths...")
+
+        base_paths = self._db_layer.get_base_paths()
+
+        base_path_entries: list = []
+
+        if base_paths is None:
+            self._state_object.service_health = \
+                ComponentDegradationLevel.FULLY_DEGRADED
+            self._state_object.database_health_state_str = "Cannot cannot " \
+                "base paths, gatherer fully degraded"
+
+        for base_path_id, base_path_path in base_paths:
+            entry: BasePathEntry = BasePathEntry(base_path_id, base_path_path)
+            base_path_entries.append(entry)
+            self._logger.debug("Cached base path: ID: %d : '%s'",
+                               base_path_id, base_path_path)
+
+        self._state.base_paths = sorted(base_path_entries, key=lambda x: x.path)
+
+        self._logger.info("Successfully cached base paths...")
+
+        return True
